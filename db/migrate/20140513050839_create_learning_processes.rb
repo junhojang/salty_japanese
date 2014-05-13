407f2c8e1da5d27b98730aa3a8de0e68677099bb
junho jang <jangjunho7@gmail.com>
2014-05-12 22:18:27 -0700

    commit 2

--- db/migrate/20140513050839_create_learning_processes.rb
+++ db/migrate/20140513050839_create_learning_processes.rb
@@ -2,7 +2,7 @@ class CreateLearningProcesses < ActiveRecord::Migration
   def change
     create_table :learning_processes do |t|
       t.integer :user_id
-      t.integr :level_test
+      t.integer :level_test
       t.integer :daily_test_cnt
       t.integer :current_reward
       t.integer :total_reward
