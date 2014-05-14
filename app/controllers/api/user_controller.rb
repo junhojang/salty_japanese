719391648afff551b34ce4b1575b6e71cc3368aa
junho jang <jangjunho7@gmail.com>
2014-05-14 03:30:23 -0700

    QQQ!

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -3,17 +3,8 @@ class Api::UserController < ApplicationController
   end
 
   def signup
-   @email=@params[:email]
-   @password=@params[:password]
-   @f_email=@params[:f_email]
-   
-   if @email.present? and @password.present?
-      @user_type=3
-      @join_method=1
-      user=User.new
-      user.email=@email
-      
-   elsif
-   end
+   @jangjunho=ENV[jangjunho]
+  end
+
   end
 end
---
+++ lib/tasks/db.rake
@@ -0,0 +1,13 @@
+namespace :db do
+  task :backup => :environment do
+    app_address = '/todpop/salty_japanise/'
+
+  end
+  
+  task :rank => :environment do
+  end
+
+end
+
+
+
