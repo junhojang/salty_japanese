e6a8b4cd6b702f5841ccc90cf12e58daf3adcf9e
junho jang <jangjunho7@gmail.com>
2014-05-14 02:36:48 -0700

    HHH

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,4 +1,19 @@
 class Api::UserController < ApplicationController
   def index
   end
+
+  def signup
+   @email=@params[:email]
+   @password=@params[:password]
+   @f_email=@params[:f_email]
+   
+   if @email.present? and @password.present?
+      @user_type=3
+      @join_method=1
+      user=User.new
+      user.email=@email
+      
+   elsif
+   end
+  end
 end
